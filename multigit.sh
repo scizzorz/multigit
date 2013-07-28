f22c7ba79d393b15e60a1d31f4403f39077710b7
John Weachock <jweachock@gmail.com>
2013-07-27 22:52:35 -0400

    Remove function definition and update recursives

--- multigit.sh
+++ multigit.sh
@@ -1,64 +1,67 @@
-function mg {
-	if [ -t 0 ]; then
-		in=~/.multigit
-	else
-		in=/dev/stdin
-	fi
+#!/bin/bash
+if [ -t 0 ]; then
+	in=~/.multigit
+else
+	in=/dev/stdin
+fi
+this=$(realpath $0)
 
-	case "$1" in
-		add)
-			shift
-			for arg in "$@"; do
-				arg=$(realpath "${arg}")
-				if [ -d "$arg/.git" ]; then
-					echo "adding $(tput setaf 2)${arg}$(tput sgr0)"
-					echo "${arg}" >> ~/.multigit
-				else
-					echo "failed to add $(tput setaf 1)${arg}$(tput sgr0)"
-				fi
-			done
-			uniq ~/.multigit | sponge ~/.multigit
-		;;
+case "$1" in
+	list)
+		cat ~/.multigit
+	;;
 
-		addr)
-			shift
-			find "$1" -name ".git" -exec realpath {}/.. \; \
-				| xargs -I {} bash -i -c "mg add {}"
-		;;
+	add)
+		shift
+		for arg in "$@"; do
+			arg=$(realpath "${arg}")
+			if [ -d "$arg/.git" ]; then
+				echo "adding $(tput setaf 2)${arg}$(tput sgr0)"
+				echo "${arg}" >> ~/.multigit
+			else
+				echo "failed to add $(tput setaf 1)${arg}$(tput sgr0)"
+			fi
+		done
+		uniq ~/.multigit | sponge ~/.multigit
+	;;
 
-		rm)
-			shift
-			for arg in "$@"; do
-				arg=$(realpath "${arg}")
-				grep -v "${arg}" ~/.multigit | sponge ~/.multigit
-				echo "removing $(tput setaf 1)${arg}$(tput sgr0)"
-			done
-		;;
+	rm)
+		shift
+		for arg in "$@"; do
+			arg=$(realpath "${arg}")
+			grep -v "${arg}" ~/.multigit | sponge ~/.multigit
+			echo "removing $(tput setaf 1)${arg}$(tput sgr0)"
+		done
+	;;
 
-		rmr)
-			shift
-			find "$1" -name ".git" -exec realpath {}/.. \; \
-				| xargs -I {} bash -i -c "mg rm {}"
-		;;
+	addr)
+		shift
+		find "$1" -name ".git" \
+			| xargs -I {} realpath "{}/.." \
+			| xargs $this add
+	;;
 
-		list)
-			cat ~/.multigit
-		;;
+	rmr)
+		shift
+		find "$1" -name ".git" \
+			| xargs -I {} realpath "{}/.." \
+			| xargs $this rm
+	;;
 
-		r)
-			shift
-			find "$1" -name ".git" -exec realpath {}/.. \; \
-				| xargs -I {} bash -i -c "echo {} | mg ${*:2}"
-		;;
+	r)
+		shift
+		find "$1" -name ".git" \
+			| xargs -I {} realpath "{}/.." \
+			| $this "${*:2}"
+	;;
 
-		*)
-			while read -r line; do
-				echo "$(tput setaf 4)${line}$(tput sgr0) $@"
-				pushd "${line}" > /dev/null
-				git "$@"
-				popd > /dev/null
-				echo
-			done < $in
-		;;
-	esac
-}
+	*)
+		while read -r line; do
+			echo "$(tput setaf 4)${line}$(tput sgr0) $@"
+			pushd "${line}" > /dev/null
+			git "$@"
+			popd > /dev/null
+			echo
+		done < $in
+	;;
+esac
